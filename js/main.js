ce1c48f1f7f5d510099dddeaab520e40929e7563
Mauro Cedes <mauro.cedes1994@gmail.com>
2023-11-12 03:06:06 -0300

    se agrego el carrito en js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 const productos = [{
-    id: 1,
+    id: 'television',
     product: 'TV LED 50"',
     thumbnail: './images/tv-led-50.webp',
     price: '629',
 },
 {
-    id: 2,
+    id: 'equipo-audio',
     product: 'Equipo de Audio 5,1 Subwoofer, Bluetooth, USB, FM/AM',
     thumbnail: './images/equipo-de-audio.webp',
     price: '149',
 },
 {
-    id: 3,
+    id: 'camara-fotos',
     product: 'Cámara de Foto FZ300 + lente 18-55mm',
     thumbnail: './images/camara-de-fotos.webp',
     price: '582',
 },
 {
-    id: 4,
+    id: 'playstation',
     product: 'PlayStation 5 825Gb Standard color blanco y negro',
     thumbnail: './images/playstation-5.webp',
     price: '839',
@@ -74,7 +74,24 @@ for (const articuloVenta of productos) {
                             <p>${articuloVenta.product}</p>
                             <p>U$S ${articuloVenta.price}</p>
                         </div>
+                        <button onclick="agregarAlCarrito('${articuloVenta.id}')">Agregar al carrito</button>
                     </article>`;
 }
 
 mainElement.appendChild(cards);
+
+const carrito = [];
+
+function agregarAlCarrito(id) {
+    const productoAgregado = productos.find((articuloVenta) => articuloVenta.id === id);
+
+    if (productoAgregado) {
+        carrito.push(productoAgregado);
+
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        console.log(carrito);
+    } else {
+        console.log(`No se encontró ningún producto con id ${id}`);
+    }
+}
+
